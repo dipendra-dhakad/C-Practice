d4445110e5ce965b9a1174b9a897bd5a4da7f766
Dipendra Dhakad <dhakaddipendradhakad@gmail.com>
2023-07-04 15:59:28 +0530

    smallest and largest in array

---
+++ Striver/Array/smallestAndLargest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int smallest(int arr[],int n){
+    int smallest =arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]< smallest)
+        {
+            smallest = arr[i];
+        }
+        
+    }
+    return smallest;
+}
+int largest(int arr[],int n){
+    int largest =arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]> largest)
+        {
+            largest = arr[i];
+        }
+        
+    }
+    return largest;
+}
+int main(){
+    int  arr[8] ={5,3,6,2,1,7,0,10} ;
+    int n=8;
+    cout<<"smallest element is " <<smallest(arr,n)<<endl;
+   cout << "largest element is " << largest(arr,n);
+    return 0 ;
+}
