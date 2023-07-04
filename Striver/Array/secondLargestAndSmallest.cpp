f1cd3278f614014bd116b593457a45a9020f41f1
Dipendra Dhakad <dhakaddipendradhakad@gmail.com>
2023-07-04 16:32:02 +0530

    Second largest and smallest element in array

---
+++ Striver/Array/secondLargestAndSmallest.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int secondLargest(int a[],int n){
+    int largest = a[0];
+    int slargest =-1;
+    for(int i =0 ;i <n;i++){
+        if(a[i]>largest){
+            slargest =largest;
+            largest = a[i];
+        }
+        else if(a[i]< largest && a[i] > slargest){
+            slargest = a[i];
+        }
+    }
+    return slargest;
+}
+
+int secondSmallest(int a[],int n){
+     int smallest = a[0];
+     int ssmallest =INT_MAX;
+     for(int i =0; i<n;i++){
+         if(a[i]<smallest){
+             ssmallest = smallest;
+             smallest =a[i];
+         }
+         else if(a[i] != smallest && a[i] <ssmallest){
+             ssmallest = a[i];
+
+         }
+     }
+     return ssmallest;
+}
+
+
+int main(){
+    int a[8] ={3,5,2,6,1,7,8,4};
+    int n=8;
+     
+    cout<<"Second largest element is " << secondLargest(a,n)<<endl;
+    cout<< "Second smallest element is " <<secondSmallest(a,n);
+    
+
+
+    return 0 ;
+}
