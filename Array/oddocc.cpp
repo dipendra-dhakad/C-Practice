4f3f0e1acebaca5812991d86aceeb08ee63b33f5
Dipendra Dhakad <dhakaddipendradhakad@gmail.com>
2023-09-08 17:44:47 +0530

    odd

--- Array/oddocc.cpp
+++ Array/oddocc.cpp
@@ -21,7 +21,7 @@ int getOddOccurrence(int arr[], int arr_size)
 int main()
 	{
 		int arr[] = { 2, 3, 5, 4,5, 2,
-					4, 3, 5, 2, 4, 4, 2 ,5,5};
+					4, 3, 5, 2, 4, 4, 2 ,5,5,7};
 		int n = sizeof(arr) / sizeof(arr[0]);
 
 		
